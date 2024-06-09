894e4ae96ce4939498f94b70e6e7343fd8348c37
codeStreakShifu <167383458+codeStreakShifu@users.noreply.github.com>
2024-06-09 14:14:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+<div class="timeline-item">
+                    <div class="timeline-dot">
+                        <div class="timeline-date">
+                            
+                        </div>
+
+                        <div class="timeline-content">
+                            
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="timeline-item">
+                    <div class="timeline-dot">
+
+                        <div class="timeline-date">
+                            2010 - 2014
+                        </div>
+
+                        <div class="timeline-content">
+                            <h3>High School</h3>
+                            <p>University of San Jose - Recoletos (2012 - 2014)</p>
+                            <p>Blessed John XXIII Seminary (2010 - 2012)</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="timeline-item">
+                    <div class="timeline-dot">
+
+                        <div class="timeline-date">
+                            2004 - 2010
+                        </div>
+
+                        <div class="timeline-content">
+                            <h3>Elementary</h3>
+                            <p>Lilo-an Norte Elementary School</p>
+                        </div>
+
+                    </div>
+                </div>
